915b28979ae56ad5dea8e26865773e2b6f73871b
Jeffrey Zampieron <jzampieron@zproject.net>
2019-06-28 15:29:17 -0400

    Add script to configure kubectl for the vagrant cluster

--- vagrant/scripts/kubectl.sh
+++ vagrant/scripts/kubectl.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-source settings.inc
+source settings.inc || exit 1
+
+USER=admin-${CLUSTER_NAME}
 
 kubectl config set-cluster ${CLUSTER_NAME} \
-    --server=https://10.240.0.20 \
+    --server=https://10.240.0.20:6443 \
     --certificate-authority=${BASEDIR}/../certs/ca.pem
+
+# TODO - do something better with the token.
+kubectl config set-credentials ${USER} \
+    --token=chAng3m3
+
+kubectl config set-context ${CLUSTER_NAME} \
+    --cluster=${CLUSTER_NAME} \
+    --user=${USER}
