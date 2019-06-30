d2e6c40f7ff1e6d1bf1e571875833180977aecf1
Jeffrey Zampieron <jzampieron@zproject.net>
2019-06-29 23:36:32 -0400

    Punt on Calico. It simply does not appear to work with the virtual box network layer

--- vagrant/terraform/networking.tf
+++ vagrant/terraform/networking.tf
@@ -11,15 +11,17 @@ resource "local_file" "calicocfg" {
     filename = "${path.cwd}/outputs/calico.yaml"
 }
 
+# Calico just won't auto-install.
+# Something is wrong with the api-server default ip address. 
 resource "null_resource" "calico" {
-    count = 1
+    count = 0
     provisioner "local-exec" {
         command = "kubectl --context=${var.k8s_cluster_context} apply -f ${path.cwd}/outputs/calico.yaml"
     }
 }
 
 resource "null_resource" "flannel" {
-    count = 0
+    count = 1
     provisioner "local-exec" {
         command = "kubectl --context=${var.k8s_cluster_context} apply -f ${path.cwd}/files/kube-flannel.yaml"
     }
